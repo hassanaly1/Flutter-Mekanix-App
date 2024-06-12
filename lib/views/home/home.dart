61a745353e1541357b4e8349871bedf7d594f6b5
hassanaly1 <your.name@domain.com>
2024-06-13 00:58:36 +0500

    AppIcon & AppName Changed for all 3 platforms.

--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -3,6 +3,7 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter_mekanix_app/helpers/custom_text.dart';
 import 'package:flutter_mekanix_app/helpers/reusable_container.dart';
 import 'package:flutter_mekanix_app/views/home/barchart.dart';
+import 'package:get/get.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -14,43 +15,26 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen>
     with SingleTickerProviderStateMixin {
   late TabController _tabController;
-  late PageController _pageController;
+  final PageController _pageController = PageController();
   final CarouselController _carouselController = CarouselController();
-  int _currentIndex = 0;
-  bool _pageChanging = false;
-  bool _carouselChanging = false;
+  final currentIndex = 0.obs;
 
   @override
   void initState() {
     super.initState();
     _tabController = TabController(length: 3, vsync: this);
-    _pageController = PageController();
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    _pageController.dispose();
-    super.dispose();
   }
 
   void _onPageChanged(int index) {
-    if (!_pageChanging && _currentIndex != index) {
-      setState(() {
-        _carouselChanging = true;
-        _currentIndex = index;
-      });
+    if (currentIndex.value != index) {
+      currentIndex.value = index;
       _carouselController.animateToPage(index);
-      _tabController.animateTo(index);
     }
   }
 
   void _onCarouselPageChanged(int index, CarouselPageChangedReason reason) {
-    if (!_carouselChanging && _currentIndex != index) {
-      setState(() {
-        _pageChanging = true;
-        _currentIndex = index;
-      });
+    if (currentIndex.value != index) {
+      currentIndex.value = index;
       _pageController.animateToPage(index,
           duration: const Duration(milliseconds: 300), curve: Curves.easeInOut);
       _tabController.animateTo(index);
@@ -76,50 +60,80 @@ class _HomeScreenState extends State<HomeScreen>
           ),
           body: Column(
             children: [
-              CarouselSlider(
-                items: [
-                  NewWidget(
-                    onTap: () => _pageController.animateToPage(0,
-                        duration: const Duration(milliseconds: 300),
-                        curve: Curves.easeInOut),
-                    title: 'Submitted Tasks',
-                    value: '34',
-                  ),
-                  NewWidget(
-                    onTap: () => _pageController.animateToPage(1,
-                        duration: const Duration(milliseconds: 300),
-                        curve: Curves.easeInOut),
-                    title: 'Total Templates',
-                    value: '12',
-                  ),
-                  NewWidget(
-                    onTap: () => _pageController.animateToPage(2,
-                        duration: const Duration(milliseconds: 300),
-                        curve: Curves.easeInOut),
-                    title: 'Total Engines',
-                    value: '8',
-                  ),
-                ],
-                options: CarouselOptions(
-                  height: 140,
-                  enlargeCenterPage: true,
-                  autoPlay: false,
-                  enableInfiniteScroll: true,
-                  viewportFraction: 0.6,
-                  onPageChanged: (index, reason) {
-                    if (reason == CarouselPageChangedReason.manual) {
-                      _onCarouselPageChanged(index, reason);
-                    }
-                  },
-                ),
-                carouselController: _carouselController,
-              ),
+              context.isPortrait
+                  ? CarouselSlider(
+                      items: [
+                        NewWidget(
+                          onTap: () => _pageController.animateToPage(0,
+                              duration: const Duration(milliseconds: 300),
+                              curve: Curves.easeInOut),
+                          title: 'Submitted Tasks',
+                          value: '34',
+                        ),
+                        NewWidget(
+                          onTap: () => _pageController.animateToPage(1,
+                              duration: const Duration(milliseconds: 300),
+                              curve: Curves.easeInOut),
+                          title: 'Total Templates',
+                          value: '12',
+                        ),
+                        NewWidget(
+                          onTap: () => _pageController.animateToPage(2,
+                              duration: const Duration(milliseconds: 300),
+                              curve: Curves.easeInOut),
+                          title: 'Total Engines',
+                          value: '8',
+                        ),
+                      ],
+                      options: CarouselOptions(
+                        height: 140,
+                        enlargeCenterPage: true,
+                        autoPlay: false,
+                        enableInfiniteScroll: true,
+                        viewportFraction: 0.5,
+                        onPageChanged: (index, reason) {
+                          if (reason == CarouselPageChangedReason.manual) {
+                            _onCarouselPageChanged(index, reason);
+                          }
+                        },
+                      ),
+                      carouselController: _carouselController,
+                    )
+                  : SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          NewWidget(
+                            onTap: () => _pageController.animateToPage(0,
+                                duration: const Duration(milliseconds: 300),
+                                curve: Curves.easeInOut),
+                            title: 'Submitted Tasks',
+                            value: '34',
+                          ),
+                          NewWidget(
+                            onTap: () => _pageController.animateToPage(1,
+                                duration: const Duration(milliseconds: 300),
+                                curve: Curves.easeInOut),
+                            title: 'Total Templates',
+                            value: '12',
+                          ),
+                          NewWidget(
+                            onTap: () => _pageController.animateToPage(2,
+                                duration: const Duration(milliseconds: 300),
+                                curve: Curves.easeInOut),
+                            title: 'Total Engines',
+                            value: '8',
+                          ),
+                        ],
+                      ),
+                    ),
               const Divider(),
               Expanded(
                 child: PageView(
                   controller: _pageController,
                   physics: const NeverScrollableScrollPhysics(),
-                  onPageChanged: _onPageChanged,
+                  onPageChanged: (index) => _onPageChanged(index),
                   children: [
                     TabView1(pageController: _pageController),
                     TabView1(pageController: _pageController),
@@ -132,6 +146,13 @@ class _HomeScreenState extends State<HomeScreen>
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    _pageController.dispose();
+    super.dispose();
   }
 }
 
